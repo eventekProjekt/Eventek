64fc00900b2d52366f6827839b1ac36afc08e560
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-12-13 11:50:35 +0100

    UserController

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+
+class UserController extends Controller
+{
+    public function index()
+    {
+        $users=response()->json(User::all());
+        return $users;
+    }
+
+    public function show($id)
+    {
+        $user=response()->json(User::find($id));
+        return $user;
+    }
+
+    public function destroy($id)
+    {
+        User::find($id)->delete();
+        return redirect('/user/list');
+    }
+
+    public function update(Request $request, $id)
+    {
+        $user=User::find($id);
+        $user->name=$request->name;
+        $user->email=$request->email;
+        $user->VIP=$request->VIP;
+        $user->password=$this->updatePassword($request, $user->id);;
+        $user->save();
+        return redirect('/user/list');
+    }
+
+    public function store(Request $request)
+    {
+        $user=new User();
+        $user->name=$request->name;
+        $user->email=$request->email;
+        $user->VIP=$request->VIP;
+        $user->password=$request->password;
+        $user->save();
+        return redirect('/user/list');
+    }
+
+    public function newView()
+    {
+        $user=User::all();
+        return view('user.new', ['user'=>$user]);
+    }
+
+    public function editView($id)
+    {
+        $user=User::find($id);
+        return view('user.edit', ['user'=>$user]);
+    }
+
+    public function listView()
+    {
+        $user = User::all();
+        return view('user.list', ['user' => $user]);
+    }
+
+    public function updatePassword(Request $request, $id)
+    {
+        $validator = Validator::make($request->all(), [
+            "password" => 'string|min:5|max:20'
+        ]);
+        if ($validator->fails()) {
+            return response()->json(["message" => $validator->errors()->all()], 400);
+        }
+        $user = User::where("id", $id)->update([
+            "password" => Hash::make($request->password),
+        ]);
+        return response()->json(["user" => $user]);
+    }
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,14 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+/*---------------------- User routes -----------------------------*/
+Route::get('/api/users', [UserController::class, 'index']);
+Route::get('/api/users/{id}', [UserController::class, 'show']);
+Route::post('/api/users/', [UserController::class, 'store']);
+Route::put('/api/user/{id}', [UserController::class, 'update']);
+Route::delete('/api/users/{id}', [UserController::class, 'destroy']);
+Route::get('/user/new', [UserController::class, 'newView']);
+Route::get('/user/edit/{id}', [UserController::class, 'editView']);
+Route::get('/user/list', [UserController::class, 'listView']);
+/*--------------------------------------------------------------*/

75c100294fd1ada43daccb813650c38dd7bb4d88
Kalandor01 <rohovszkyakoska@gmail.com>
2022-12-13 14:25:58 +0100

    törlt js

--- public/main.js
+++
@@ -1,3 +0,0 @@
-import './js/controller/EventsAdminController.js';
-import './js/controller/EventsPublicController.js';
-import './js/controller/ParticipatesAdminControler.js';
--- resources/js/main.js
+++
@@ -1,3 +0,0 @@
-import '../../public/js/controller/EventsAdminController.js';
-import '../../public/js/controller/EventsPublicController.js';
-import '../../public/js/controller/ParticipatesAdminControler.js';

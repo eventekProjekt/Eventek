a78fdb0d088a244f05ad826c9b7dc0e06ee8106b
Kalandor01 <rohovszkyakoska@gmail.com>
2022-12-13 11:46:08 +0100

    event és participate controler + routes

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\EventsController;
+use App\Http\Controllers\ParticipatesController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +18,23 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//event endpoints
+Route::get('/api/events', [EventsController::class, 'index']);
+Route::get('/api/events/{id}', [EventsController::class, 'show']);
+Route::post('/api/events/', [EventsController::class, 'store']);
+Route::put('/api/event/{id}', [EventsController::class, 'update']);
+Route::delete('/api/events/{id}', [EventsController::class, 'destroy']);
+Route::get('/event/new', [EventsController::class, 'newView']);
+Route::get('/event/edit/{id}', [EventsController::class, 'editView']);
+Route::get('/event/list', [EventsController::class, 'listView']);
+
+//participate end points
+Route::get('/api/participates', [ParticipatesController::class, 'index']);
+Route::get('/api/participates/{id}', [ParticipatesController::class, 'show']);
+Route::post('/api/participates/', [ParticipatesController::class, 'store']);
+Route::put('/api/participate/{id}', [ParticipatesController::class, 'update']);
+Route::delete('/api/participates/{id}', [ParticipatesController::class, 'destroy']);
+Route::get('/participate/new', [ParticipatesController::class, 'newView']);
+Route::get('/participate/edit/{id}', [ParticipatesController::class, 'editView']);
+Route::get('/participate/list', [ParticipatesController::class, 'listView']);

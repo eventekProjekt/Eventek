56ae0ddc9ce3d032ec7d83dabf019b2b9961f7cc
daniellaszlo123 <daniellaszlo002@gmail.com>
2022-12-13 14:45:07 +0100

    menupontok kattintas

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,28 @@
 import './controller/EventsAdminController.js';
+import EventsAdminController from './controller/EventsAdminController.js';
 import './controller/EventsPublicController.js';
+import EventsPublicController from './controller/EventsPublicController.js';
 import './controller/ParticipatesAdminControler.js';
+import ParticipatesAdminControler from './controller/ParticipatesAdminControler.js';
+
+document.querySelector(".eventAdmin").addEventListener("click", ()=>{
+    const szuloelem=document.querySelector("article");
+    szuloelem.innerHTML=""
+    //const admE = new EventsAdminController();
+    szuloelem.innerText="event admin oldal"
+    
+})
+document.querySelector(".participants").addEventListener("click", ()=>{
+    const szuloelem=document.querySelector("article");
+    szuloelem.innerHTML=""
+    //const admP = new ParticipatesAdminControler();
+    szuloelem.innerText="participants admin oldal"
+    
+})
+document.querySelector(".event").addEventListener("click", ()=>{
+    const szuloelem=document.querySelector("article");
+    szuloelem.innerHTML=""
+    //const usrE = new EventsPublicController();
+    szuloelem.innerText="events public oldal"
+    
+})
